5f0b7d72477eb607b36054ce0d2378cfcdc50b3e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-24 12:46:08 +0900

    render index.html

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.LoadHTMLGlob("static/*.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{})
 	})
 	r.Run(":8000")
 }
